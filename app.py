e42562658015e6f9abe625fba2735c6e919c80aa
DeathPaw <gleb.ramazanov.2014@mail.ru>
2021-09-23 12:49:55 +0300

    add /weather

--- app.py
+++ app.py
@@ -14,17 +14,46 @@ app = Flask(__name__)
 def weather_dashboard():
     return render_template('home.html')
 
+@app.route("/weather")
+def weather():
+    city = request.args.get('city')
+    days_number = int(request.args.get('days'))  
+    response = calculate_result(city, days_number)
+    print(response)
+    return response
+
 @app.route('/output', methods=['POST'])
 def test1():
-    today = date.today()
+    
     days_number = request.form['days_number']
-    city = request.form['city']
+    city = request.form['city']     
+    response = calculate_result(city, days_number)
+    return my_json
+
+def get_weather(today, end_date, api_key, city):
+    url = "https://visual-crossing-weather.p.rapidapi.com/history"
+    request_start_date = str(today)+"T00:00:00"
+    request_end_date = str(end_date)+"T00:00:00"
+    querystring = {"startDateTime":request_end_date,"aggregateHours":"24","location":city,"endDateTime":request_start_date,"unitGroup":"us","dayStartTime":"8:00:00","contentType":"json","dayEndTime":"17:00:00","shortColumnNames":"0"}
+
+    headers = {
+        'x-rapidapi-host': "visual-crossing-weather.p.rapidapi.com",
+        'x-rapidapi-key': api_key
+        }
+    
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    return response.json()
+
+def get_api_key():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config['openweathermap']['x-rapidapi-key']
+
+def calculate_result(city, days_number):
+    today = date.today()
     end_date = today - timedelta(days=int(days_number))
-    #print (city)
-    #print (days_number)
     api_key = get_api_key()
-    data = get_weather(today, end_date, api_key, int(city))
-       
+    data = get_weather(today, end_date, api_key, city)
     day=0
     humidity=[day]*int(days_number)
     temp=[day]*int(days_number)
@@ -55,31 +84,7 @@ def test1():
          "humidity": { "average": avg_humidity, "median": med_humidity, "min": min_humidity, "max": max_humidity },
          "pressure_mb": {"average": avg_pressure, "median": med_pressure, "min": min_pressure, "max": max_pressure } }
     my_json=json.dumps(res)
-    #print (my_json)
-    ###MY ENDPOINT
-    #return render_template('weather.html',
-    #                       location=location, temp=temp,
-    #                       humidity=humidity, sealevelpressure=sealevelpressure, start_date = end_date, end_date = today)
     return my_json
-
-def get_weather(today, end_date, api_key, city):
-    url = "https://visual-crossing-weather.p.rapidapi.com/history"
-    request_start_date = str(today)+"T00:00:00"
-    request_end_date = str(end_date)+"T00:00:00"
-    querystring = {"startDateTime":request_end_date,"aggregateHours":"24","location":int(city),"endDateTime":request_start_date,"unitGroup":"us","dayStartTime":"8:00:00","contentType":"json","dayEndTime":"17:00:00","shortColumnNames":"0"}
-
-    headers = {
-        'x-rapidapi-host': "visual-crossing-weather.p.rapidapi.com",
-        'x-rapidapi-key': api_key
-        }
-    
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    return response.json()
-
-def get_api_key():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    return config['openweathermap']['x-rapidapi-key']
 
 
 if __name__ == '__main__':
